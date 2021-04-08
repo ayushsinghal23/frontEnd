026fcc3acdc48e0e3e595faf98d2653df2c0ccc2
Ayush Singhal <ayush.singhal_cs18@gla.ac.in>
2021-04-08 20:28:18 +0530

    initial commit

---
+++ idealogee/src/urlConfig.js
@@ -0,0 +1,7 @@
+/** @format */
+const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+
+export const api = "http://localhost:2000/api";
+export const genratePublicUrl= (fileName) =>{
+ return `http://localhost:2000/public/${fileName}`
+}
